3492437687ce9405bff58d23161b81ff7ef83a76
Kaushik Nimalan <66821715+kaushiknimalan@users.noreply.github.com>
2020-07-07 21:13:27 +0530

    Exception example

---
+++ test.dart
@@ -0,0 +1,54 @@
+import 'dart:io';
+
+int mustgreaterthanZero(int val){
+if(val <= 0) {
+  throw Exception('Value must be greater than zero');
+  }
+  return val;
+}
+
+void letVerifyTheValue(var val){
+  var  valueVerification;
+
+  try {
+    valueVerification = mustgreaterthanZero(val);
+  }
+  catch(e){
+    print(e);
+  }
+  finally{
+    if (valueVerification == null){
+      print('Value is not accepted');
+    }
+    else {
+      print('Value verified: $valueVerification');
+    }
+  }
+}
+
+void main(){
+  print("Enter the first number.");
+  var one = int.parse(stdin.readLineSync());
+ 
+  print(" ");
+
+
+  print("Enter the second number.");
+  var two = int.parse(stdin.readLineSync());
+
+
+  print(" ");
+
+   print("Enter the three number.");
+  var three = int.parse(stdin.readLineSync());
+  
+
+  print("  ");
+
+  letVerifyTheValue(one);
+  print(" ");
+  letVerifyTheValue(two);
+  print(" ");
+  letVerifyTheValue(three);
+  print(" ");
+}
